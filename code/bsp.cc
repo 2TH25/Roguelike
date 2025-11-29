f841c9a6f189615385dce5fa5273c7a8ea9acfaa
Kilian <kiliangaillard25@gmail.com>
2025-11-29 16:08:39 +0100

    ajout du constructeur BSPTree

--- code/bsp.cc
+++ code/bsp.cc
@@ -3,8 +3,19 @@
 #include "bsp.h"
 
 
-bool BSPTree::split(gf::Random& random, int leafSizeMinimum) { // cas de base de la méthode split
+
+BSPTree::BSPTree(gf::RectI initialSpace) { // constructeur du BSPTree
+    left = nullptr;
+    right = nullptr;
+    space = initialSpace;
+}
+
+bool BSPTree::split(gf::Random& random, int leafSizeMinimum) { // méthode split
     if(left||right) { // si le noeud en question a déjà un fils gauche ou droite, alors on ne peut pas le split
         return false; // on renvoie faux
     }
+
+    bool splitHorizontally = random.computeBernoulli(0.5f);
+    // renvoie true ou false, 50% du temps l'un et 50% l'autre
+    //selon la loi de Bernouilli (true = p et false = 1 - p)
 }
--- code/bsp.h
+++ code/bsp.h
@@ -8,9 +8,16 @@ struct BSPTree{
     gf::RectI space; // l'espace correspondant au rectangle
     gf::RectI room; // la salle dans le rectangle
 
+
+    // voici la strucuture RectI pour mieux se la représenter :
+    // struct RectI {
+    //     gf::Vector2i position; // x, y
+    //     gf::Vector2i size;     // w, h
+    // };
+
     BSPTree(gf::RectI initialSpace);
 
-    bool split(gf::Random& random, int leafSizeMinimum); // cas de base de la méthode pour split
+    bool split(gf::Random& random, int leafSizeMinimum); // méthode pour split
     void recursiveSplit(gf::Random& random, int leafSizeMinimum, int leafSizeMaximum); // split récursif
     void createRooms(gf::Random& random, int roomSizeMinimum, int roomSizeMaximum); // création des salles
 
