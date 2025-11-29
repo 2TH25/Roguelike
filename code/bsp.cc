ea878e5237b332dc76673148f2ecfafe04f5afe9
Kilian <kiliangaillard25@gmail.com>
2025-11-29 16:34:06 +0100

    ajout de getters et continuité de la méthode split

--- code/bsp.cc
+++ code/bsp.cc
@@ -10,6 +10,17 @@ BSPTree::BSPTree(gf::RectI initialSpace) { // constructeur du BSPTree
     space = initialSpace;
 }
 
+//getters de largeur et hauteur pour BSPTree
+int BSPTree::getWidth() const { 
+    return space.getSize().x;
+}
+
+int BSPTree::getHeight() const {
+    return space.getSize().y;
+}
+
+
+
 bool BSPTree::split(gf::Random& random, int leafSizeMinimum) { // méthode split
     if(left||right) { // si le noeud en question a déjà un fils gauche ou droite, alors on ne peut pas le split
         return false; // on renvoie faux
@@ -18,4 +29,18 @@ bool BSPTree::split(gf::Random& random, int leafSizeMinimum) { // méthode split
     bool splitHorizontally = random.computeBernoulli(0.5f);
     // renvoie true ou false, 50% du temps l'un et 50% l'autre
     //selon la loi de Bernouilli (true = p et false = 1 - p)
+
+    if(space.getWidth() >= 1.25 * space.getHeight()) {
+        splitHorizontally = false; // on force à faux la division horizontale 
+        // si le rectangle est déjà trop large
+        
+    }
+    else if (space.getHeight() >= 1.25 * space.getWidth()){
+        splitHorizontally = true;
+        //sinon si le rectangle est déjà trop haut, alors on s'assure de 
+        // le diviser horizontalement
+
+    }
+
+
 }
--- code/bsp.h
+++ code/bsp.h
@@ -1,9 +1,11 @@
 
 // code basique aidé du github de gf, intitulé : "DungeonGenerator_BinarySpacePartitioning.h"
 
+#include <gf/Rect.h>
+
 struct BSPTree{
-    std::unique_ptr<ArbreBSP> left;
-    std::unique_ptr<ArbreBSP> right; // les deux fils d'un noeud splité
+    std::unique_ptr<BSPTree> left;
+    std::unique_ptr<BSPTree> right; // les deux fils d'un noeud splité
 
     gf::RectI space; // l'espace correspondant au rectangle
     gf::RectI room; // la salle dans le rectangle
@@ -16,6 +18,10 @@ struct BSPTree{
     // };
 
     BSPTree(gf::RectI initialSpace);
+
+
+    int getWidth() const;
+    int getHeight() const;
 
     bool split(gf::Random& random, int leafSizeMinimum); // méthode pour split
     void recursiveSplit(gf::Random& random, int leafSizeMinimum, int leafSizeMaximum); // split récursif
