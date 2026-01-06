250388e700576124793798c236215c4abd2ee8f0
Samia <sbenali.68000@gmail.com>
2026-01-06 16:44:30 +0100

    Gestion du character

--- code/bits/Entity.h
+++ code/bits/Entity.h
@@ -2,16 +2,21 @@
 #define ENTITY_H
 
 #include <string>
-
 #include <gf/Color.h>
 #include <gf/Vector.h>
 
 namespace rCMI {
 
   struct Entity {
+    Entity() = default;
+
+    Entity(gf::Vector2i pos, char16_t ch, gf::Color4f col, std::string n, bool blocks)
+      : position(pos), character(ch), color(col), name(std::move(n)), blocks_movement(blocks)
+    {}
+
     gf::Vector2i position = { 0, 0 };
-    char16_t character = ' ';
-    gf::Color color = White;
+    char16_t character = u' ';
+    gf::Color4f color = gf::Color::White; 
     std::string name;
     bool blocks_movement = false;
   };
