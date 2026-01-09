c8ccb5f4e722e1871f4984f9a78aa17890e8bd06
Samia <sbenali.68000@gmail.com>
2026-01-09 12:16:32 +0100

    mort des mobs avec tombe

--- code/bits/DungeonGenerator.h
+++ code/bits/DungeonGenerator.h
@@ -1,42 +1,42 @@
-#ifndef DUNGEON_GENERATOR_H
-#define DUNGEON_GENERATOR_H
+// #ifndef DUNGEON_GENERATOR_H
+// #define DUNGEON_GENERATOR_H
 
-#include <gf/Array2D.h>
-#include <gf/Random.h>
-#include <gf/Vector.h>
-#include "Tile.h"
-#include "bsp.h"
+// #include <gf/Array2D.h>
+// #include <gf/Random.h>
+// #include <gf/Vector.h>
+// #include "Tile.h"
+// #include "bsp.h"
 
-namespace rCMI {
+// namespace rCMI {
 
-  using Dungeon = gf::Array2D<TileType, int>;
+//   using Dungeon = gf::Array2D<TileType, int>;
 
-  class DungeonGenerator {
-  public:
-    enum class Phase {
-      Start,
-      Iterate,
-      Finish,
-    };
+//   class DungeonGenerator {
+//   public:
+//     enum class Phase {
+//       Start,
+//       Iterate,
+//       Finish,
+//     };
 
-    DungeonGenerator();
-    virtual ~DungeonGenerator();
+//     DungeonGenerator();
+//     virtual ~DungeonGenerator();
 
-    Phase getPhase() const {
-      return m_phase;
-    }
+//     Phase getPhase() const {
+//       return m_phase;
+//     }
 
-    void setPhase(Phase phase) {
-      m_phase = phase;
-    }
+//     void setPhase(Phase phase) {
+//       m_phase = phase;
+//     }
 
-    virtual Dungeon generate(gf::Random& random) = 0;
+//     virtual Dungeon generate(gf::Random& random) = 0;
 
-  private:
-    Phase m_phase;
-  };
+//   private:
+//     Phase m_phase;
+//   };
 
-}
+// }
 
 
-#endif
+// #endif
--- code/bits/bsp.h
+++ code/bits/bsp.h
@@ -1,64 +1,63 @@
-#ifndef BSP_H
-#define BSP_H
+// #ifndef BSP_H
+// #define BSP_H
 
-// code basique aidé du github de gf, intitulé : "DungeonGenerator_BinarySpacePartitioning.h"
+// // code basique aidé du github de gf, intitulé : "DungeonGenerator_BinarySpacePartitioning.h"
 
-#include <gf/Rect.h>
-#include <memory>
-#include <gf/Random.h>
+// #include <gf/Rect.h>
+// #include <memory>
+// #include <gf/Random.h>
 
-#include "DungeonGenerator.h"
-#include "Tile.h"
-#include "GameData.h"
+// #include "DungeonGenerator.h"
+// #include "Tile.h"
 
-namespace rCMI {
+// namespace rCMI {
 
-    struct BSPTree{
-        std::unique_ptr<BSPTree> left;
-        std::unique_ptr<BSPTree> right; // les deux fils d'un noeud splité
+//     struct BSPTree{
+//         std::unique_ptr<BSPTree> left;
+//         std::unique_ptr<BSPTree> right; // les deux fils d'un noeud splité
 
-        gf::RectI space; // l'espace correspondant au rectangle
-        gf::RectI room; // la salle dans le rectangle
-
-
-        // voici la strucuture RectI pour mieux se la représenter : 
-        // struct RectI {
-        //     gf::Vector2i position; // x, y
-        //     gf::Vector2i size;     // w, h
-        // };
-
-        BSPTree(gf::RectI initialSpace);
+//         gf::RectI space; // l'espace correspondant au rectangle
+//         gf::RectI room; // la salle dans le rectangle
 
 
-        int getWidth() const;
-        int getHeight() const;
+//         // voici la strucuture RectI pour mieux se la représenter : 
+//         // struct RectI {
+//         //     gf::Vector2i position; // x, y
+//         //     gf::Vector2i size;     // w, h
+//         // };
 
-        bool split(gf::Random& random, int leafSizeMinimum); // méthode pour split
-        void recursiveSplit(gf::Random& random, int leafSizeMinimum, int leafSizeMaximum); // split récursif
-        void createRooms(gf::Random& random, int RoomMinSize, int RoomMaxSize); // création des salles
+//         BSPTree(gf::RectI initialSpace);
 
-    };
 
-    class BSP : public DungeonGenerator {
-        public: 
-        BSP();
+//         int getWidth() const;
+//         int getHeight() const;
 
-        int leafSizeMinimum = 10;
-        int leafSizeMaximum = 24;
+//         bool split(gf::Random& random, int leafSizeMinimum); // méthode pour split
+//         void recursiveSplit(gf::Random& random, int leafSizeMinimum, int leafSizeMaximum); // split récursif
+//         void createRooms(gf::Random& random, int RoomMinSize, int RoomMaxSize); // création des salles
 
-        Dungeon generate(gf::Random& random) override;
+//     };
 
-        private:
-        gf::Random m_savedRandom;
-        gf::Random m_random;
-        BSPTree m_root;
-        Dungeon m_dungeon;
-        void generateRooms();
-        void walkTree(const BSPTree& tree);
-        void createRoom(const gf::RectI& room);
-        void createHorizontalTunnel(int x1, int x2, int y);
-        void createVerticalTunnel(int x, int y1, int y2);
-    };
-}
+//     class BSP : public DungeonGenerator {
+//         public: 
+//         BSP();
 
-#endif
+//         int leafSizeMinimum = 10;
+//         int leafSizeMaximum = 24;
+
+//         Dungeon generate(gf::Random& random) override;
+
+//         private:
+//         gf::Random m_savedRandom;
+//         gf::Random m_random;
+//         BSPTree m_root;
+//         Dungeon m_dungeon;
+//         void generateRooms();
+//         void walkTree(const BSPTree& tree);
+//         void createRoom(const gf::RectI& room);
+//         void createHorizontalTunnel(int x1, int x2, int y);
+//         void createVerticalTunnel(int x, int y1, int y2);
+//     };
+// }
+
+// #endif
